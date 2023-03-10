fa617a247b1f71d3d9fd31a02d8f1c6150dbb3ae
김은비 <>
2023-03-10 16:30:55 +0900

    React로 Todolist 만들기

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+// import Button from "./Button";
+// import styles from "./App.module.css";
+import { useState } from "react";
+
 
 function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(toDo === "") {
+      return;
+    }
+    setToDo(""); // toDo 초기화
+    setToDos(currentArray => [toDo, ...currentArray])  // state에 있는 todo와 이전의 모든 todo를 가져온다.
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> TO DO LIST !</h1>
+      <b> 📝 오늘 할 일 : {toDos.length} 개</b>
+      <form onSubmit={onSubmit}>
+        <input 
+          onChange={onChange} 
+          value = {toDo} 
+          type= "text" 
+          placeholder="할 일을 적어보세요!"
+        />
+        <button> 작성 </button> 
+      </form>
+      <hr/>
+      <ul>
+        {toDos.map((item, index) => ( 
+            <li key={index}> {item}</li>
+          ))}
+      </ul>
     </div>
+      
   );
 }
 
